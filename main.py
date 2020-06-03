b89c72879105a207444e951b63c1bd48e69afd69
walecu1999 <maciekht@interia.pl>
2020-06-03 21:16:28 +0200

    Zrobienie planszy

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
-
+from plansza import Plansza
 # Uruchomienie programu
 pygame.init()
 
 #Pionki
-bialy= pygame.image.load("assets/bialy_pionek.png")
-czarny= pygame.image.load("assets/czarny_pionek.png")
+bialy = pygame.image.load("assets/bialy_pionek.png")
+czarny = pygame.image.load("assets/czarny_pionek.png")
 gracz1x = 200
 gracz1y = 200
 gracz2x = 100
@@ -14,28 +14,36 @@ gracz2y=150
 #def pionek():
 
 #def damka():
-
-
 def gracz1():
     ekran.blit(bialy, (gracz1x, gracz1y))
 def gracz2():
     ekran.blit(czarny, (gracz2x, gracz2y))
 # Ekran gry
-szerokosc=600
-wysokosc=1200
-kolor=(205,205,205)
+szerokosc=800
+wysokosc=800
+kolor_tla=(10,150,30)
 ekran = pygame.display.set_mode((wysokosc,szerokosc))
+planszowka= Plansza(wysokosc,szerokosc,ekran)
+
 
 #Tytul i ikona
 pygame.display.set_caption("Warcaby Polskie by Maciej Walczyk")
 ikona = pygame.image.load("assets/ikona.png")
 pygame.display.set_icon(ikona)
+
+
 wlaczony = 1
 while wlaczony:
-    ekran.fill(kolor)
+    ekran.fill(kolor_tla)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             wlaczony = 0
     gracz1()
     gracz2()
+    planszowka.rysuj()
     pygame.display.update()
+
+def checkboard(x):
+    list = [[(j + i) % 2 for j in range(1, x + 1)] for i in range(x)]
+    return list
+
--- plansza.py
+++ plansza.py
@@ -1,15 +1,70 @@
-class plansza(object):
+import pygame
+
+class Plansza(object):
     CZARNY = 1
     BIALY = 0
     NOTDONE = -1
-    def __init__(self,wysokosc,szerokosc, gracz1):
+    KOLUMNY = 10
+    WIERSZE = 10
+    Biale_pole = (0, 0, 0)
+    Czarne_pole = (205, 205, 205)
+    def __init__(self,wysokosc,szerokosc,ekran):
+        self.ekran=ekran
         self.wysokosc=wysokosc
         self.szerokosc=szerokosc
-        self.polabialych = []
-        self.polaczarnych = []
-        for i in range(szerokosc)
-            self.polaczarnych.append((i,(i+1)%2))
-            self.polabialych.append((i,wysokosc - (i%2)-1))
-        self.status = [[' ']* self.szerokosc for x in range(self.wysokosc)]
+        self.pola_bialych = []
+        self.pola_czarnych = []
+        self.gracze=['C','B'];
+        self.tablica = [['-', 'C', '-', 'C', '-', 'C', '-', 'C', '-','C'],
+                        ['C', '-', 'C', '-', 'C', '-', 'C', '-', 'C', '-'],
+                        ['-', 'C', '-', 'C', '-', 'C', '-', 'C', '-', 'C'],
+                        ['C', '-', 'C', '-', 'C', '-', 'C', '-', 'C', '-'],
+                        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                        ['-', 'B', '-', 'B', '-', 'B', '-', 'B', '-', 'B'],
+                        ['B', '-', 'B', '-', 'B', '-', 'B', '-', 'B', '-'],
+                        ['-', 'B', '-', 'B', '-', 'B', '-', 'B', '-', 'B'],
+                        ['B', '-', 'B', '-', 'B', '-', 'B', '-', 'B', '-']]
+
+
+        for i in range(szerokosc):
+            self.pola_czarnych.append((i, (i + 1) % 2))
+            self.pola_bialych.append((i, wysokosc - (i % 2) - 1))
+        self.status = [[' ' for i in range(self.szerokosc)] for x in range(self.wysokosc)]
         self.wygrana = self.NOTDONE
-        self.turn = gracz1
+
+
+    def checkboard(x):
+        list = [[(j + i) % 2 for j in range(1, x + 1)] for i in range(x)]
+        return list
+    def tworz_plansze(self):
+        tablica = [[i for kolumna in range(self.KOLUMNY)]
+                   for wiersz in range(self.WIERSZE)]
+        return tablica
+
+    def rysuj(self):
+        #kolor_planszy=self.checkboard()
+        k=0
+        x, y = 0,0
+        a=0
+        for i in range(0,self.WIERSZE):
+            for j in range(0,self.KOLUMNY):
+                if (k+a)%2:
+                    k=k+1
+                    pygame.draw.rect(self.ekran,self.Czarne_pole, pygame.Rect(x,y,self.szerokosc/10,self.wysokosc/10))
+                else:
+                    k=k+1
+                    pygame.draw.rect(self.ekran,self.Biale_pole, pygame.Rect(x, y, self.szerokosc / 10, self.wysokosc / 10))
+                x+=self.szerokosc/10
+            y+=self.wysokosc/10
+            x = 0
+            a+=1
+        czcionka= pygame.font.SysFont("timesnewroman",20,bold = True)
+        #czcionka_kolor= BLACK_COLOR
+
+    def draw_background(self):
+        '''
+        Draws background for the board
+        '''
+        #font= pygame.font.SysFont("timesnewroman",20,bold = True)
+        #pygame.draw.rect(self.game.screen, LIGHT_GREY_COLOR, pygame.Rect(0, 0, self.width, self.height))
